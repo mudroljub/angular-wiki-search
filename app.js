2dc83dee52b6d96f2048211df7393322991840c7
mudroljub <mudroljub@gmail.com>
2015-10-19 17:06:56 +0200

    searchWikipedia

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 (function () {
+
+	// napraviti funkciju koja pretrazuje strane
+	// objediniti funkcije da se obe pozivaju iz jedne na svaku promenu
+	// ako ima jedan rezultat, prikazuje jedan, ako ima vise lista ih
+
 	'use strict';
 	angular
 		.module("wikiModul", ['ngSanitize'])
@@ -10,11 +15,22 @@
 		var wiki = this;
 		wiki.term = 'Buddha';
 		wiki.json = {};	// samo u razvoju, posle obrisati
-		wiki.page = {};
+		wiki.page = null;
+		wiki.results = [];
 		wiki.error = "";
 
-		wiki.findPage = function () {
-			var paramUrl = createParamUrl(wiki.term);
+
+		/*** PUBLIC METHODS ***/
+
+		wiki.loadPage = function (searchTerm) {
+
+			var params = {
+				action: 'query',
+				prop: 'extracts|pageimages|pageterms',
+				redirects: '',
+				titles: searchTerm
+			};
+			var paramUrl = createParamUrl(params, searchTerm);
 
 			$http.jsonp(paramUrl)
 				.success(function (data) {
@@ -28,30 +44,44 @@
 					}
 				})
 				.error(function(){
-					wiki.error = "Oh no, there is some error.";
+					wiki.error = "Oh no, there was some error in geting data.";
 				});
 
-		}; // findPage
+		}; // loadPage
 
 
-
-		// HELPER FUNCTIONS
-
-		function createParamUrl(term) {
-			var apiUrl = 'http://en.wikipedia.org/w/api.php';
+		wiki.searchWikipedia = function(searchTerm){
 
 			var params = {
 				action: 'query',
-				prop: 'extracts|pageimages|pageterms',
-				redirects: '',
-				titles: term,
-				format: 'json',
-				formatversion: 2
+				list: 'search',
+				srsearch: searchTerm
 			};
+			var paramUrl = createParamUrl(params, searchTerm);
 
-			var paramString = serialize(params);
-			var fullParamUrl = apiUrl + '?' + paramString + '&callback=JSON_CALLBACK';
-			return fullParamUrl;
+			$http.jsonp(paramUrl)
+				.success(function (data) {
+					wiki.json = data.query;
+					wiki.results = data.query.search;
+				})
+				.error(function(){
+					wiki.error = "Oh no, there was some error in geting data.";
+				});
+
+		};	// searchWikipedia
+
+
+
+		/*** HELPER FUNCTIONS ***/
+
+		function createParamUrl(params) {
+			var apiUrl = 'http://en.wikipedia.org/w/api.php';
+			// default params for all
+			params.format = 'json';
+			params.formatversion = 2;
+			params.callback = 'JSON_CALLBACK';
+			var paramUrl = apiUrl + '?' + serialize(params);
+			return paramUrl;
 		} // createParamUrl
 
 		function serialize(params) {
